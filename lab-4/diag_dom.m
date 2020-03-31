bcb3ae79d3452d02e3825443d229ed800e2db151
Subhalingam D <subhalingam.d@gmail.com>
2020-03-31 12:05:11 +0530

    lab 4 (incomplete)

---
+++ lab-4/diag_dom.m
@@ -0,0 +1,10 @@
+function res = diag_dom(A)
+    %DIAG_DOM Summary of this function goes here
+    %   Detailed explanation goes here
+    res=1;
+    [m,n]=size(A);
+    for i=1:m
+
+    end
+end
+
